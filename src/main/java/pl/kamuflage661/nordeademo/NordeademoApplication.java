843afd0635454a327fa0327b2815347afba5e480
Marcin Wiankowski <Marcin.Wiankowski-External@hamburgsud.com>
2017-11-27 17:17:11 +0100

    Added primary commit

---
+++ src/main/java/pl/kamuflage661/nordeademo/NordeademoApplication.java
@@ -0,0 +1,12 @@
+package pl.kamuflage661.nordeademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordeademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NordeademoApplication.class, args);
+	}
+}
